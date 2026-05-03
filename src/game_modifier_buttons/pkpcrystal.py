8b0c264ab59e68f6e7f047633a8eba4810fbc74d
Giancarlo <15658199+gVirtu@users.noreply.github.com>
2026-05-02 22:50:06 -0300

    fix (pkpcrystal): prevent running shoes modifier when script is running

--- src/game_modifier_buttons/pkpcrystal.py
+++ src/game_modifier_buttons/pkpcrystal.py
@@ -14,8 +14,9 @@ def _run_condition(controller) -> bool:
     """Disable B button from running if in a battle."""
     pyboy = controller.pyboy
     mode = symbol_read_u8(pyboy, "wBattleMode")
+    script_running = symbol_read_u8(pyboy, "wScriptRunning")
 
-    return BattleMode(mode) == BattleMode.NONE
+    return BattleMode(mode) == BattleMode.NONE and script_running == 0
 
 MODIFIER_BUTTONS = [
     ModifierButtonSpec(
